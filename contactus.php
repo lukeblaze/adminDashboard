1e6fdba2c28b23bd45561b1907f3410b99e5bd1b
lukeblaze <blazemurimi@gmail.com>
2022-07-15 12:11:58 +0300

    added a new php syntax

--- contactus.php
+++ contactus.php
@@ -1,56 +1,67 @@
 <?php
-require_once('connection.php');
+require_once('logics/dbconnection.php');
 //1.database connection
-$sqlFetchEnrollmentStudent= mysqli_query($conn, "SELECT * FROM contactus");
-while($fetchStudent= mysqli_fetch_array($sqlFetchEnrollmentStudent))
-{
-    $firstname = $fetchStudent['firstname'];
-    $lastname = $fetchStudent['lastname'];
-    $email = $fetchStudent['email'];
-    $phonenumber = $fetchStudent['phonenumber'];
-    $message = $fetchStudent['message'];
-    $created_at = $fetchStudent['created_at'];
-    
-}
+$sqlFetchContactStudent= mysqli_query($conn, "SELECT * FROM contactus ");
+while($fetchStudent= mysqli_fetch_array($sqlFetchContactStudent))
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php require_once('includes/header.php')?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.2.0-beta1-dist/bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <!-- All our code here -->
+    <?php require_once('includes/navbar.php')?>
+    <?php require_once('includes/sidebar.php')?>
+<div class="main-content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-hover table-stripped table-responsive{-sm|-md|-lg}" style="font-size:12px">
+                    <thead>
+                        <div class="card-header bg-dark text-white text-center">
+                            <span>Contact Us</span>
+                        </div>
 
-            <body>
-                <?php require_once('includes/navbar.php')?>
-                    <?php require_once('includes/sidebar.php')?>
-                         <div class="main-content pt-5">
-                             <div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                    <div class="card">
-                                        <div class="card-header bg-dark text-white text-center">
-                                            <div class="card-title"><h4>Contact Us</h4></div>
-                                        </div>
-                                        <div class="card-body" style="font-size:12px">
-                                            <div class="list-group">
-                                                <li class="list-group-item">Firstname: <span class="float-end"><?php echo $firstname?></span></li>
-                                                <li class="list-group-item">Lastname: <span class="float-end"><?php echo $lastname?></span></li>
-                                                <li class="list-group-item">Email: <span class="float-end"><?php echo $email?></span></li>
-                                                <li class="list-group-item">Phonenumber: <span class="float-end"><?php echo $phonenumber?></span></li>
-                                                <li class="list-group-item">Message: <span class="float-end"><?php echo $message?></span></li>
-                                                <li class="list-group-item">Sent at: <span class="float-end"><?php echo $created_at?></span></li>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    </div>
-                                    </div>
-                                </div>
 
-                             </div>
-                         </div>
-   
-                    <?php require_once('includes/scripts.php')?>
-            </body>
+                        <tr>
+                            <th scope="col">First name</th>
+                            <th scope="col">Last name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone Number</th>
+                            <th scope="col">Message</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       <?php while($fetchContactRecord= mysqli_fetch_array($sqlFetchContactStudent)) { ?>
+                        <tr>
+                            <td><?php echo $fetchContactRecord['firstname']?></td>
+                            <td><?php echo $fetchContactRecord['lastname']?></td>
+                            <td><?php echo $fetchContactRecord['email']?></td>
+                            <td><?php echo $fetchContactRecord['phonenumber']?></td>
+                            <td><?php echo $fetchContactRecord['message']?></td>
+                            <td><?php echo $fetchContactRecord['created_at']?></td>
+                           
+                           
+                        </tr>
+                       <?php }  ?>
+                    </tbody>                        
+                    </table>
+                </div>
+            </div>
+        </div>
 
-    </head>
-</html>
+    </div>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
+    </html>
